cce44a53c24dda48864d1b867e5c13ac5bd70d86
sergii_yastremskyi <theyastremsky@gmail.com>
2022-09-29 22:04:58 +0300

    lazzt state init

--- src/App.js
+++ src/App.js
@@ -12,19 +12,18 @@ import Filter from './components/filter';
 
 
 export default function App() {
-  const [contacts, setContacts] = useState(JSON.parse(window.localStorage.getItem('contacts')) ??
-    [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(()=>{return JSON.parse(window.localStorage.getItem('contacts')) ?? [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]});
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
   const [filter, setFilter] = useState('');
    
   useEffect(() => { window.localStorage.setItem('contacts', JSON.stringify(contacts)) }, [contacts]);
-  // useEffect(() =>{})
+
 
   const formSubmitHandler = (name,number) => {
     const isExist = contacts.some(contact => {
